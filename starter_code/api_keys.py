4e0c554a360ee0c7a43613b3c1fd53805099bc33
Rachel <rachelaburime@gmail.com>
2021-02-09 21:00:04 -0600

    Recorded weather from 627 cities

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "2da701bd8b370196009a200f0b08d988"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDYx9eskGMdcftDwTLiGrNul1-9MXK2LwQ"
